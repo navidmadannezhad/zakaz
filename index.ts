d93cb5c007171de99dab83751c504135721859ef
navidmadannezhad <navidproject283@gmail.com>
2025-05-08 22:46:10 +0330

    feat: add the recursive function

--- index.ts
+++ index.ts
@@ -2,28 +2,32 @@ import {
     readdir,
     writeFile
 } from "fs/promises";
-import { fileExists, generateExportContent, getFolderEntitiesFromPathEntities } from "./utils";
+import { fileExists, generateExportContent, getFolderEntitiesFromPathEntities, isDirectory } from "./utils";
 import type { PathEntity } from "./types";
 
-const TARGET_DIRECTORY: string = "./test/configs";
-const RESTRICTED_DIRECTORIES: string[] = []; 
+const TARGET_DIRECTORY: string = "./test/components";
+const RESTRICTED_DIRECTORIES: string[] = [
+    'api', 'assets'
+]; 
 
 const generateOrdersInDirectory = async (path: string): Promise<void> => {
-
     const pathEntities: PathEntity[] = await readdir(path);
     const folders = await getFolderEntitiesFromPathEntities(path, pathEntities);
 
-    const data = await generateExportContent(path, pathEntities);
-    console.log(data)
-
     const indexExists = await fileExists(`${path}/index.ts`);
-
     const content = await generateExportContent(path, pathEntities);
 
     if(!indexExists) await writeFile(`${path}/index.ts`, content, { 
         flag: indexExists ? "a" : "w"
     });
 
+    for(const folderPath of folders){
+        if(!RESTRICTED_DIRECTORIES.find(dir => path.endsWith(dir))){
+            generateOrdersInDirectory(folderPath)
+        }
+    }
 }
 
-await generateOrdersInDirectory(TARGET_DIRECTORY);
+await generateOrdersInDirectory(
+    TARGET_DIRECTORY
+);
