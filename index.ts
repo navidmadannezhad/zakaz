0c90dbcaa65c34ad34cf732e24c02877c12ae19f
navidmadannezhad <navidproject283@gmail.com>
2025-05-09 10:03:35 +0330

    feat: add initial commands to work

--- index.ts
+++ index.ts
@@ -1,19 +1,21 @@
+#!/usr/bin/env bun
+
 import {
     readdir,
-    writeFile
 } from "fs/promises";
-import { fileExists, generateExportContent, getFolderEntitiesFromPathEntities, isDirectory, populateIndexFile } from "./utils";
+import { 
+    generateExportContent, 
+    getFolderEntitiesFromPathEntities, 
+    populateIndexFile 
+} from "./utils";
 import type { PathEntity } from "./types";
+import { Command } from "commander";
 
-const TARGET_DIRECTORY: string = "./test/components";
 const RESTRICTED_DIRECTORIES: string[] = [
     'api', 'assets'
 ]; 
 
-// when there is a currently index.tsx file, the content shouldn't be overrided, and the repeated lines shouldn't be generated
-// turn this code into a library
-
-const generateOrdersInDirectory = async (path: string): Promise<void> => {
+const generateOrdersInDirectory = async (path: string, restricted_dirs: string[] = []): Promise<void> => {
     const pathEntities: PathEntity[] = await readdir(path);
     const folders = await getFolderEntitiesFromPathEntities(path, pathEntities);
     const content = await generateExportContent(path, pathEntities);
@@ -26,6 +28,29 @@ const generateOrdersInDirectory = async (path: string): Promise<void> => {
     }
 }
 
-await generateOrdersInDirectory(
-    TARGET_DIRECTORY
-);
+const program = new Command();
+
+program
+    .name('zakaz')
+    .description('Simple CLI to order your named exports and default exports in an index.ts file')
+    .version('1.0.0')
+    .option('-r, --restricted-dirs <restricted_dirs>', 'restricted paths', "")
+    .option('-b, --entry-path <entry_path>', 'Entry Path', './')
+    .action(async (options) => {
+        await generateOrdersInDirectory(
+            options.entryPath,
+            options.restrictedDirs.replaceAll(' ', '').split(",")
+        )
+    });
+
+program
+    .command('test')
+    .description('Test')
+    .action(async (_options) => {
+        await generateOrdersInDirectory(
+            "./test/configs",
+            ['api', 'assets']
+        )
+    });
+
+program.parse();
